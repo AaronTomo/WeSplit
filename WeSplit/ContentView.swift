dd457346ae14870d9e3efdf15727309cf198c0b9
Aaron Thompson <aaronseanthompson@icloud.com>
2024-06-17 17:50:35 +0100

    Repo clones and getting started for day 16 of 100daysofswiftui

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var name = "Aaron"
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Text(name)
         }
         .padding()
     }
