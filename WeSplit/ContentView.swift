e347549647a5860244bb0e3e43e706a3a6b26dd9
Aaron Thompson <aaronseanthompson@icloud.com>
2024-06-18 21:51:50 +0100

    Day 17 of a 100 days of Swift. For this one i decided to try and create the app myself and then watch the videos which is why it looks a bit different from Pauls but I did follow his code on the last video to hide the keyboard haha. Still needs polish  though

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,18 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var name = "Aaron"
+    @State var cost: Double = 0
+    @State var numberOfPeople = 0
+    @State var tipPercent: Int = 20
+    
+    @FocusState private var amountInFocus: Bool
+    
+    let percentages = [5, 10, 15, 20, 0]
+    
+  
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            
-            Text(name)
+        
+        NavigationView{
+            Form{
+                
+                
+                Section{
+                    TextField("\(cost)", value: $cost, format: .currency(code: "GBP")).keyboardType(.decimalPad).focused($amountInFocus)
+                    
+                    TextField("Number of people", value: $numberOfPeople, format: .number).keyboardType(.numberPad)
+                }
+                
+                
+
+                
+                Section{
+                    
+                    Picker("Tip percentage", selection: $tipPercent){
+                        ForEach(percentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                
+                Section{
+                    
+                    Text("Each person owes \(  Double( (cost / 100 * Double(tipPercent)) + cost )  / Double(numberOfPeople) )")
+                }
+                
+            }.navigationTitle("WeSplit")
+                .toolbar{ if amountInFocus {
+                    Button("Done"){
+                        amountInFocus = false
+                    }
+                }}
         }
-        .padding()
+        
     }
 }
 
